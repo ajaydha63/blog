08dbb74ab4b70de10ee5292d2af8fc63ae1632ba
Ajay <ajay.dha63@gmail.com>
2021-10-06 13:39:57 -0700

    posts fix

--- app.py
+++ app.py
@@ -3,14 +3,23 @@ app = Flask(__name__)
 
 posts = [
     {
-        
+        'author': 'Kabibi',
+        'title': 'Who is the best QB in the NFL right now?',
+        'content': 'Ive been wondering who the best Qb is for a whole becasue i want to know who to get in fantasy football but i just cant figure it out.',
+        'date_posted': 'october 6th, 2021'
+    },
+    {
+        'author': 'Denius',
+        'title': ' Response to - Who is the best QB in the NFL right now?',
+        'content': 'If i were you i would take Kyler Murray he is a dual threat and front runner or MVP and a great pickup.',
+        'date_posted': 'october 7th, 2021'
     }
 ]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
